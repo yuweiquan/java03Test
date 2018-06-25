41327ae95738ffdc839ac8fd3e14f636deb21000
鱼尾泉 <39145430+yuweiquan@users.noreply.github.com>
2018-06-25 15:12:41 +0800

    第一个文件
    这个是详细信息,里面进行了首页的处理

---
+++ hello.java
@@ -0,0 +1,14 @@
+public class HellWorld {
+
+	public static void main(String[] args) {
+		System.out.println("哈哈哈哈哈");
+		System.out.println("huqhuujwhfjhwj");
+		System.out.println("1234546476");
+		int  a=10;
+		int b=10;
+		System.out.println(a+b);
+		System.out.println("你是小乌龟");
+
+	}
+
+}
